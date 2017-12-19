8b3c4ffe888f3cdbff001d6d628b67a94221a9a6
chrisdean258 <chrisdean258@gmail.com>
2017-12-19 02:29:10 -0500

    Simplified fetch vimrc script to remove user input update script control in favor of .vim/auto_update/fetch_vimrc.sh

--- fetch_vimrc.sh
+++ fetch_vimrc.sh
@@ -14,14 +14,9 @@ DIFF=`echo $NOW - $LAST_UPDATE | bc`
 
 if [ $# -eq 0 ] || ( [ $1="auto" ] && [ $DIFF -gt 7 ] ) ; then
 	Tabs=`cat $MY_VIMRC | grep -e "^\s:set tabstop=.$" | cut -d = -f 2`
-	Update_command=`cat vimrc.bak | grep -e "let update_script" | cut -d \" -f 2`
 
 	cat $MY_VIMRC | grep -e "^\" :highlight Folded ctermfg" &> /dev/null
 	Highlight=$?
-
-	#please dont ask -- hopefully i can fix it better later
-	UPDATE_COMMAND=`cat $MY_VIMRC | grep -e "let update_script" | cut -d \" -f 2 | sed -e "s/\//\\\\\\\\\\//g"`
-
 
 	wget --backups=3 https://raw.githubusercontent.com/chrisdean258/vimrc/master/vimrc -O $MY_VIMRC || exit 1
 
@@ -33,8 +28,6 @@ if [ $# -eq 0 ] || ( [ $1="auto" ] && [ $DIFF -gt 7 ] ) ; then
 	if [ $Highlight -eq 1 ]; then
 		sed -i "s/^\" :highlight Folded/:highlight Folded/" $MY_VIMRC
 	fi
-
-	sed -i "s/.*let update_script.*/\t:let update_script= \"$UPDATE_COMMAND\"/" $MY_VIMRC
 
 	sed -i "s/^LAST_UPDATE=.*/LAST_UPDATE=${NOW}/" $THIS_FILE
 fi
